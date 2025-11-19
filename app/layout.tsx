a9808f62f3f2c6d560dbb7383ef36134b8f468b2
bodybykishani <lumban.vjme@gmail.com>
2025-11-19 11:57:21 +0800

    Updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Van Lumban',
-  description: 'Android Developer'
+  title: 'Body by Kishani',
+  description: 'Fitness Coach'
 }
 
 export default function RootLayout({
